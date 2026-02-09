5bd511bdaded9d1e65d39a4422586aee3bfc6d06
hlaclau <hugo.laclau@gmail.com>
2026-02-10 00:13:43 +0100

    feat: sketchybar tokyo dark theme

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Simple Black Theme
-export WHITE=0xFFFFFFFF
-export GREY=0xFFAAAAAA
-export DARK_GREY=0xFF666666
+# Tokyo Dark Theme
+export WHITE=0xFFa0a8cd
+export GREY=0xFF565f89
+export DARK_GREY=0xFF3b4261
 
-export BLACK=0xFF000000
-export BAR_COLOR=0xEE000000
-export ITEM_BG_COLOR=0xFF1a1a1a
-export ACCENT=0xFFFFFFFF
+export BLACK=0xFF06080a
+export BAR_COLOR=0xEE06080a
+export ITEM_BG_COLOR=0xFF11121d
+export ACCENT=0xFFa485dd
 
 export TRANSPARENT=0x00000000
